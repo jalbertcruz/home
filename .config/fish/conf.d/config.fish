33ca1681f67dc1e4c8804b1bc09ebe00175b1cc7
José Albert Cruz Almaguer <jalbertcruz@gmail.com>
2021-05-19 16:15:19 +0200

    update

--- .config/fish/conf.d/config.fish
+++ .config/fish/conf.d/config.fish
@@ -1,112 +1,143 @@
 
-function g
-    command git $argv
+set fish_function_path $fish_function_path "/usr/share/powerline/bindings/fish"
+source /usr/share/powerline/bindings/fish/powerline-setup.fish
+
+
+#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
+set -x NVM_DIR "$HOME/.nvm"
+bass source "$NVM_DIR/nvm.sh" ';' # This loads nvm
+
+#bass source ~/.nvm/nvm.sh --no-use ';' 
+
+function nvm
+  nvm $argv
 end
+
+# theme
+set -g theme_color_scheme terminal-dark
+set -g fish_prompt_pwd_dir_length 1
+set -g theme_display_user yes
+set -g theme_hide_hostname no
+set -g theme_hostname always
+set -gx TERM xterm-256color
+
+powerline-setup
 
 function mux 
     command tmuxinator $argv
 end
 
 alias ... "cd ../../"
+alias g git
+alias e exa
 
-set PLANTUML_HOME /home/a/appslnx/tools/plantuml
-set PLANTUML_ICON_FONT_SPRITES /home/a/appslnx/tools/plantuml/plantuml-icon-font-sprites
+set -x PLANTUML_HOME /home/a/appslnx/tools/plantuml
+set -x PLANTUML_ICON_FONT_SPRITES /home/a/appslnx/tools/plantuml/plantuml-icon-font-sprites
 
-set KUBECONFIG /home/a/src/a/tmbz/k8s/stg_es/k8s.cfg /home/a/src/a/tmbz/k8s/dev_es/k8s.cfg
+set -xU --path KUBECONFIG /home/a/src/a/tmbz/k8s/stg_es/STG-K8S02-user.cfg /home/a/src/a/tmbz/k8s/dev_es/k8s.cfg
 
-set NOMAD_ADDR 'http://172.16.10.65:4646'
-set NOMAD_REGION global
-set NOMAD_NAMESPACE '*'
-set NOMAD_SKIP_VERIFY yes
+set -x NOMAD_ADDR 'http://172.16.10.65:4646'
+set -x NOMAD_REGION global
+set -x NOMAD_NAMESPACE '*'
+set -x NOMAD_SKIP_VERIFY yes
 
 
-set PIPENV_VENV_IN_PROJECT 1
-set PIPENV_IGNORE_VIRTUALENVS 1
+set -x PIPENV_VENV_IN_PROJECT 1
+set -x PIPENV_IGNORE_VIRTUALENVS 1
 
-set RIPGREP_CONFIG_PATH $HOME/.ripgreprc
+set -x RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 
-set LD_LIBRARY_PATH /usr/local/lib/
+set -x LD_LIBRARY_PATH /usr/local/lib/
 
-set ANSIBLE_HOST_KEY_CHECKING False
+set -x ANSIBLE_HOST_KEY_CHECKING False
 
 # source /media/a/data/installers/Unix/CLI-tools/tmuxinator/completion/tmuxinator.fish
 
 
-set HEX_UNSAFE_HTTPS true
-set HEX_HTTP_TIMEOUT 1000
+set -x HEX_UNSAFE_HTTPS true
+set -x HEX_HTTP_TIMEOUT 1000
 
-set DEVPI_SERVERDIR /media/a/data/repo/pypi/server/
-set HEX_HOME /media/a/data/repo/.hex/
+set -x DEVPISERVER_SERVERDIR /media/a/data/repo/pypi/server/
+set -x HEX_HOME /media/a/data/repo/.hex/
 
-set LEIN_JVM_OPTS "-Dmaven.wagon.rto=1800000"
+set -x LEIN_JVM_OPTS "-Dmaven.wagon.rto=1800000"
 
-set BOOT_CLOJURE_NAME org.clojure/clojure
-set BOOT_VERSION 2.8.1
-set BOOT_CLOJURE_VERSION 1.9.0
-set BOOT_HOME /media/a/data/repo/.boot
+set -x BOOT_CLOJURE_NAME org.clojure/clojure
+set -x BOOT_VERSION 2.8.1
+set -x BOOT_CLOJURE_VERSION 1.9.0
+set -x BOOT_HOME /media/a/data/repo/.boot
 
-set BOOT_LOCAL_REPO /media/a/data/repo/internet2/boot.m2
+set -x BOOT_LOCAL_REPO /media/a/data/repo/internet2/boot.m2
 
-set BOOT_CLOJARS_REPO https://localhost/repository/clojars
-set BOOT_MAVEN_CENTRAL_REPO https://localhost/repository/maven-public
+set -x BOOT_CLOJARS_REPO https://localhost/repository/clojars
+set -x BOOT_MAVEN_CENTRAL_REPO https://localhost/repository/maven-public
 
-set GROOVY_HOME ~/appslnx/groovy
+set -x GROOVY_HOME ~/appslnx/groovy
 
-set SBT_OPTS "-Xms1512M -Xmx3024M -Xss200M -XX:MaxMetaspaceSize=2024M"
+set -x SBT_OPTS "-Xms1512M -Xmx3024M -Xss200M -XX:MaxMetaspaceSize=2024M"
 
-set SBT_CREDENTIALS /media/a/data/repo/internet2/.credentials
+set -x SBT_CREDENTIALS /media/a/data/repo/internet2/.credentials
 
 
-set GRADLE_USER_HOME /media/a/data/repo/internet2/.gradle
+set -x GRADLE_USER_HOME /media/a/data/repo/internet2/.gradle
 
-set COURSIER_CACHE /media/a/data/repo/internet2/.coursier
+set -x COURSIER_CACHE /media/a/data/repo/internet2/.coursier
 
-set JAVA_HOME ~/appslnx/jdk-11
+set -x JAVA_HOME ~/appslnx/jdk-11
 
-set PATH $HOME/src/devOps/architecture/tools $HOME/.mix/escripts $HOME/appslnx/tools/jbake/bin/ $HOME/appslnx/scala/scala-2.13/bin/ $GROOVY_HOME/bin $HOME/appslnx/monitoring/prometheus-2.9.x/ $HOME/appslnx/web/krakend $HOME/appslnx/monitoring/containers-exporter/ $HOME/appslnx/monitoring/grok_exporter-0.x/ $PATH
+set -x PATH $HOME/src/devOps/architecture/tools $HOME/.mix/escripts $HOME/appslnx/tools/jbake/bin/ $HOME/appslnx/scala/scala-2.13/bin/ $GROOVY_HOME/bin $HOME/appslnx/monitoring/prometheus-2.9.x/ $HOME/appslnx/web/krakend $HOME/appslnx/monitoring/containers-exporter/ $HOME/appslnx/monitoring/grok_exporter-0.x/ $PATH
 
-set PATH $HOME/appslnx/jdk-11/bin/ $HOME/appslnx/build-tools/maven/bin/ $HOME/.rbenv/shims/ $PATH
+set -x PATH $HOME/appslnx/jdk-11/bin/ $HOME/appslnx/build-tools/maven/bin/ $PATH
+#set -x PATH $HOME/appslnx/jdk-11/bin/ $HOME/appslnx/build-tools/maven/bin/ $HOME/.rbenv/shims/ $PATH
 
-set PATH ~/.kiex/bin/ $PATH
+# https://github.com/rbenv/rbenv
+set -Ux fish_user_paths $HOME/.rbenv/bin $fish_user_paths
+status --is-interactive; and source (rbenv init -|psub)
+
+set -x PATH ~/.kiex/bin/ $PATH
  
 
-source $HOME/.kiex/elixirs/.elixir-1.11.4.env.fish
+set -xU KERL_CONFIGURE_OPTIONS --with-microstate-accounting=extra --enable-vm-probes --with-dynamic-trace=dtrace --with-javac --enable-hipe --enable-kernel-poll --without-odbc --enable-threads --enable-sctp --enable-smp-support
 
-. /home/a/kerl/23.2/activate.fish
+#source $HOME/.kiex/elixirs/.elixir-1.11.4.env.fish
+source $HOME/.kiex/elixirs/.elixir-1.12.1.env.fish
+#. /home/a/kerl/23.2/activate.fish
+. /home/a/kerl/24.0/activate.fish
 
-set KERL_CONFIGURE_OPTIONS "--disable-debug --without-javac"
-set ERL_AFLAGS "-kernel shell_history enabled"
+set -x ERL_AFLAGS "-kernel shell_history enabled"
 
-set PATH ~/appslnx/build-tools/sbt/bin $PATH
-set PATH ~/appslnx/bin $PATH
-# set PATH ~/.local/bin:$PAT
+set -x PATH ~/appslnx/build-tools/sbt/bin $PATH
+set -x PATH ~/appslnx/bin $PATH
+# set -x PATH ~/.local/bin:$PAT
 
 
-set PATH ~/appslnx/build-tools/gradle/bin $PATH
+set -x PATH ~/appslnx/build-tools/gradle/bin $PATH
 
-set PATH $HOME/bin $PATH
-set PATH $HOME/appslnx/git/p4/bin/ $PATH
+set -x PATH $HOME/bin $PATH
+set -x PATH $HOME/appslnx/git/p4/bin/ $PATH
 
-set PATH /usr/local/git/bin $PATH
+set -x PATH /usr/local/git/bin $PATH
 
 # Golang
-set GOROOT $HOME/appslnx/google/go
-set PATH $GOROOT/bin $PATH
-set GOPATH $HOME/appslnx/go/path
-set PATH $GOPATH/bin $PATH
+set -x GOROOT $HOME/appslnx/google/go
+set -x PATH $GOROOT/bin $PATH
+set -x GOPATH $HOME/appslnx/go/path
+set -x PATH $GOPATH/bin $PATH
 
 # Julia
-set PATH ~/appslnx/opt/julia/bin/ $PATH
+set -x PATH ~/appslnx/opt/julia/bin/ $PATH
 
 # Rust
-set PATH ~/appslnx/opt/rust-1/bin/ $PATH
+set -x PATH ~/appslnx/opt/rust-1/bin/ $PATH
 
 
-set PATH ~/.rbenv/bin /opt/google/chrome $HOME/appslnx/cp_prolog/bin/x86_64_linux $PATH
+set -x PATH /opt/google/chrome $HOME/appslnx/cp_prolog/bin/x86_64_linux $PATH
+#set -x PATH ~/.rbenv/bin /opt/google/chrome $HOME/appslnx/cp_prolog/bin/x86_64_linux $PATH
 
-set PATH $HOME/.pub-cache/bin $HOME/appslnx/go/path/bin/ $PATH
+set -x PATH $HOME/.pub-cache/bin $HOME/appslnx/go/path/bin/ $PATH
 
-set EDITOR vim
-set VISUAL vim
+set -x EDITOR vim
+set -x VISUAL vim
 
 direnv hook fish | source
